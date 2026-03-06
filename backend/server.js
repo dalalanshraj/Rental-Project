2bc5255f26a6992f472f856da64b5a86e9406fd0
dalalanshraj <dalalanshraj@gmail.com>
2026-03-07 01:11:13 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,8 @@ const PORT = process.env.PORT || 8000;
 const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
-   "http://localhost:5173",
-    "https://mydesign.blog/"
+  "http://localhost:5173",
+  "https://mydesign.blog"
 ];
 
 app.use(
